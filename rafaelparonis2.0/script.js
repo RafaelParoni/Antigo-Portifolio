67593b45bd6281ca68656daee89a1cffb11e2289
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2023-07-31 19:51:53 -0300

    website update
    update my personal website

---
+++ rafaelparonis2.0/script.js
@@ -0,0 +1,104 @@
+    function scroll(value){
+        const element = document.getElementById(value);
+        
+        element.scrollIntoView();
+    }
+    function alerta(value){
+        if(value == "discord"){
+            document.getElementById('alertdiscord').style.display = "block"
+        }else{
+            document.getElementById('alert').style.display = "block"
+        }
+    }
+    function notConfirm(value){
+        if(value == 'discord'){
+            document.getElementById('alertdiscord').style.display = "none"
+        }else{
+            document.getElementById('alert').style.display = "none"
+        }
+        
+    }
+    function confirm(value){
+        window.location = document.getElementById('confirm').getAttribute('value')
+        noConfirm()
+    }
+    function red(value){
+        if(value == 'instagram'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://www.instagram.com/rafaelparroni/")
+        }else if(value == 'github'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://github.com/RafaelParoni")
+        }else if(value == 'twitter'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://twitter.com/RafaelParoni")
+        }else if(value == 'youtube'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://www.youtube.com/channel/UCCjwv7_RJteoSRhbRqFqP-Q")
+        }else if(value == 'discord'){
+            alerta('discord')
+        }else if(value == 'html'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://g.co/kgs/cNYSe2")
+        }else if(value == 'css'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://g.co/kgs/yKs1if")
+        }else if(value == 'js'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://g.co/kgs/nJhcNE")
+        }else if(value == 'php'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://g.co/kgs/co6VH6")
+        }else if(value == 'mysql'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://g.co/kgs/JcqJmV")
+        }else if(value == 'python'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://g.co/kgs/z8zt1L")
+        }else if(value == 'terminal'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://github.com/RafaelParoni/Website-DB")
+        }else if(value == 'alert'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://github.com/RafaelParoni/alert")
+        }else if(value == 'snake-game'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://github.com/RafaelParoni/snake-game")
+        }else if(value == 'website-old'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://github.com/RafaelParoni/website-old")
+        }else if(value == 'website-pessoal'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://github.com/RafaelParoni/Website-pessoal")
+        }else if(value == 'alert-preview'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://alertcode.glitch.me/")
+        }
+        else if(value == 'login-screen'){
+            alerta()
+            var elementValue = document.getElementById('confirm')
+            elementValue.setAttribute("value", "https://github.com/RafaelParoni/login_screen")
+        }
+    }
+    function project(value){
+        window.location = "./projeto/" + value + ".html"
+    }
+    function goBack(){
+        window.location = '../index.html'
+    }
